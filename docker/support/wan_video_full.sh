d79f89c74de08916c63b30b6abacef1627b0ed97
Dodzilla <scottdods@gmail.com>
2025-03-18 16:39:51 -0700

    Update wan_video_full.sh

--- docker/support/wan_video_full.sh
+++ docker/support/wan_video_full.sh
@@ -54,6 +54,8 @@ LORA_MODELS=(
     "https://furgen-models.b-cdn.net/models/male-masturbation-90.safetensors"
     "https://furgen-models.b-cdn.net/models/wan_missionary_side.safetensors"
     "https://furgen-models.b-cdn.net/models/wan_pov_blowjob.safetensors"
+    "https://furgen-models.b-cdn.net/models/wan_doggy_POV_v1_1.safetensors"
+    "https://furgen-models.b-cdn.net/models/wan_doggy_side.safetensors"
 )
 
 # WanVideo VAE
