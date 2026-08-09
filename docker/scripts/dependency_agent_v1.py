ddac9922334c564c0542f53963566823af2d8a7f
Dodzilla <scottdods@gmail.com>
2026-08-09 14:27:02 -0700

    Release dependency agent 0.10.118

--- docker/scripts/dependency_agent_v1.py
+++ docker/scripts/dependency_agent_v1.py
@@ -130,7 +130,7 @@ from pathlib import Path
 from typing import Any, Callable, Dict, Iterable, List, Optional, Set, Tuple
 
 
-AGENT_VERSION = "dm-agent-py/0.10.117"
+AGENT_VERSION = "dm-agent-py/0.10.118"
 VIDEO_GEN_V2_FURGENPUB_COMMIT = "f46d81937e578aaf6f2674cd5deb7982ea09b4bb"
 VIDEO_GEN_V2_FURGENPUB_RAW_BASE_URL = (
     f"https://raw.githubusercontent.com/Dodzilla/FurgenPub/{VIDEO_GEN_V2_FURGENPUB_COMMIT}/docker/support"
